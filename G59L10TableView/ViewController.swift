63f4d13eeb8381044b2dd25ef03d034ef45d1ce0
Ivan Besarab <Ivan.Besarab@RockSoft.rocks>
2018-01-09 21:05:51 +0200

    protocol implemented

--- G59L10TableView/ViewController.swift
+++ G59L10TableView/ViewController.swift
@@ -8,14 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 	
 	@IBOutlet weak var tableView: UITableView!
+	
+	var cars = [String]()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		cars = ["e28", "e30", "e36"]
+		tableView.dataSource = self
 		tableView.backgroundColor = .blue
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return cars.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = UITableViewCell()
+		return cell
 	}
 	
 
