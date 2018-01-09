b1dc7e544cab5eb4fa5972bb92a3da9f797b80af
Ivan Besarab <Ivan.Besarab@RockSoft.rocks>
2018-01-09 20:26:30 +0200

    tableView ready to work from code

--- G59L10TableView/ViewController.swift
+++ G59L10TableView/ViewController.swift
@@ -9,16 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var tableView: UITableView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		tableView.backgroundColor = .blue
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
+	
 
 
 }
