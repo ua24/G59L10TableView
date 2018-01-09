b2b2681f6f7d9fce11df4a66b47e1f8a991c27a4
Ivan Besarab <Ivan.Besarab@RockSoft.rocks>
2018-01-09 19:47:25 +0200

    Initial Commit

---
+++ G59L10TableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  G59L10TableView
+//
+//  Created by Ivan Vasilevich on 1/9/18.
+//  Copyright © 2018 RockSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
